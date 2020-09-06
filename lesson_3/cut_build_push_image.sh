9f39bf8937f93a1d2b51b6c029582b0ebef734fe
Dario Lencina <dario@armore.dev>
2020-09-05 21:35:55 -0400

    save

--- lesson_3/cut_build_push_image.sh
+++ lesson_3/cut_build_push_image.sh
@@ -3,8 +3,6 @@ IMAGE_URL=gcr.io/project-name/app-name:$(git rev-parse HEAD)
 
 echo "Uploading new image to "$IMAGE_URL
 
-yarn build
-
 docker build -t rust_json_api_prod . -f Dockerfile.production
 
 docker tag garage $IMAGE_URL
